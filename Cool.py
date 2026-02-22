8efdee0e186388e5223b16196dcafd82b5c08abb
UtterlyUselessTech™ <supreme243athome@gmail.com>
2026-02-22 22:31:46 +0500

    Delete Cool.py

--- Cool.py
+++
@@ -1,8 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import webbrowser
-
-port = 470
-server_adress = ("", port)
-httpd = HTTPServer(server_adress, SimpleHTTPRequestHandler)
-webbrowser.get().open_new_tab("http://localhost:{port}")
-httpd.serve_forever()
